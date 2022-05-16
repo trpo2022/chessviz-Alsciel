41b3009552bbfc5b91408a9fd2e2052575b1a373
Alsciel <aleksander_silantev@mail.ru>
2022-05-16 19:34:52 +0700

    Add libs

---
+++ src/libchessviz/chessviz.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define boardsize 8
+
+void CreateBoard(char board[boardsize][boardsize]);
+void PrintBoard(char c[boardsize][boardsize]);
+void Error();
+void ErrorCode(int n);
+int ParseX(char n);
+int ParseY(char n);
+int CheckWhite(char board[boardsize][boardsize]);
+int CheckBlack(char board[boardsize][boardsize]);
+void WhitePawn(char board[boardsize][boardsize], char w[7]);
+void BlackPawn(char board[boardsize][boardsize], char w[7]);
+void WhiteKing(char board[boardsize][boardsize], char w[7]);
+void BlackKing(char board[boardsize][boardsize], char w[7]);
+void WhiteRook(char board[boardsize][boardsize], char w[7]);
+void BlackRook(char board[boardsize][boardsize], char w[7]);
+void WhiteBishop(char board[boardsize]boardsize], char w[7]);
+void BlackBishop(char board[boardsize][boardsize], char w[7]);
+void WhiteKnight(char board[boardsize][boardsize], char w[7]);
+void BlackKnight(char board[boardsize][boardsize], char w[7]);
+void WhiteQueen(char board[boardsize][boardsize], char w[7]);
+void BlackQueen(char board[boardsize][boardsize], char w[7]);
+void White(char board[boardsize][boardsize], char w[7]);
+void Black(char board[boardsize][boardsize], char b[7]);
+int WinWhite(char board[boardsize][boardsize], char w[7], int n);
+int WinBlack(char board[boardsize][boardsize], char w[7], char b[7], int n);
+
+int w_pass = 0;
+int w_x = -1;
+int b_pass = 0;
+int b_x = -1;
---
+++ src/libchessviz/print_board.c
@@ -0,0 +1,58 @@
+#include <libchessviz/chessviz.h>
+
+extern int w_pass;
+extern int w_x;
+extern int b_pass;
+extern int b_x;
+
+void CreateBoard(char board[boardsize][boardsize])
+{
+    char mas[boardsize][boardsize] = {
+            {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+            {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+            {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+    };
+    for (int i = 0; i < boardsize; i++) {
+        for (int j = 0; j < boardsize; j++) {
+            board[i][j] = mas[i][j];
+        }
+    }
+}
+
+void PrintBoard(char c[boardsize][boardsize])
+{
+    printf("\n");
+    for (int i = 0; i < boardsize; i++) {
+        printf("%d  | ", boardsize - i);
+        for (int j = 0; j < boardsize; j++)
+            printf("%d ", boardrr[i][j]);
+        printf("\n");
+    }
+    printf("  +-----------------\n");
+    printf("    a b c d e f g h\n");
+}
+
+void Error()
+{
+    printf("\n Invalid input \n");
+}
+void ErrorCode(int n)
+{
+    printf("\n Invalid input, code: %d \n", n);
+}
+
+int ParseX(char n)
+{
+    int x = n - 97;
+    return x;
+}
+int ParseY(char n)
+{
+    int y = n - 49;
+    return y;
+}
